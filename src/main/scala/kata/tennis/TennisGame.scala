9931fc53c946795dba0a26f294d0c015d2daf6e2
Ivan Lorenz <ivan.lorenz@availpro.com>
2015-10-16 17:09:50 +0200

    Some minor refactor

--- src/main/scala/kata/tennis/TennisGame.scala
+++ src/main/scala/kata/tennis/TennisGame.scala
@@ -1,44 +1,43 @@
 package kata.tennis
 
 sealed trait Player
-case class Player1() extends Player
-case class Player2() extends Player
+case object Player1 extends Player
+case object Player2 extends Player
 
-sealed trait Score
-case class Love() extends Score
-case class Fifteen() extends Score
-case class Thirty() extends Score
-case class Forty() extends Score
-case class Wins() extends Score
-case class Advantage() extends Score
+sealed trait Score {
+  def wins(looser: Score): (Score, Score) = this match {
+    case Love => (Fifteen, looser)
+    case Fifteen => (Thirty, looser)
+    case Thirty => (Forty, looser)
+    case Forty if looser == Advantage => (Forty, Forty)
+    case Forty if looser != Forty => (Love, Love)
+    case Forty if looser == Forty => (Advantage, looser)
+    case Advantage if looser != Advantage => (Love, Love)
+    case Advantage if looser == Advantage => (Forty, Forty)
+  }
+}
+
+case object Love extends Score
+case object Fifteen extends Score
+case object Thirty extends Score
+case object Forty extends Score
+case object Wins extends Score
+case object Advantage extends Score
 
 case class TennisGame(player1: Score, player2: Score) {
 
   def play(winner: Player): TennisGame = {
     winner match {
-      case Player1() if points(player1,player2)._1 != Wins() => {
-        val p = points(player1, player2)
+      case Player1 => {
+        val p = player1 wins player2
         TennisGame(p._1, p._2)
       }
-      case Player2() if points(player2,player1)._1 != Wins() => {
-        val p = points(player2, player1)
+      case Player2 => {
+        val p = player2 wins player1
         TennisGame(p._2, p._1)
       }
-      case _ => TennisGame(Love(), Love())
     }
   }
 
-  private def points(winner: Score, looser: Score): (Score, Score) = winner match {
-    case Love() => (Fifteen(), looser)
-    case Fifteen() => (Thirty(), looser)
-    case Thirty() => (Forty(), looser)
-    case Forty() if looser == Advantage() => (Forty(), Forty())
-    case Forty() if looser != Forty() => (Wins(), looser)
-    case Forty() if looser == Forty() => (Advantage(), looser)
-    case Advantage() if looser != Advantage() => (Wins(), looser)
-    case Advantage() if looser == Advantage() => (Forty(), Forty())
-  }
-
 }
-
 
